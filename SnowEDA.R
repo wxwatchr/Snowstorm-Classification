27f6d91d740cdbec66d1112b7afa5e916cd6e545
Mallory Burford <wxwatchr@gmail.com>
2020-06-08 14:29:10 -0500

    Add files via upload

--- SnowEDA.R
+++ SnowEDA.R
@@ -11,7 +11,7 @@ ggplot(data=snow,aes(x=reorder(STATE,EVENT_TYPE),y=EVENT_TYPE)) +
 ## plot Event Types by Year
 plotOne <- table(snow$EVENT_TYPE, snow$YEAR)
 barplot(plotOne, main="Event Type by Year",
-        xlab="Year", col=c("darkblue","red"),
+        xlab="Year", ylab="Number of Occurences", col=c("lightblue","red", "black", "green", "purple", "orange", "darkblue", "Yellow"),
         legend = rownames(snow$EVENT_TYPE))
 
 ## plot d1 by Year
@@ -23,49 +23,49 @@ barplot(plotTwo, main="Day One Snow Totals by Year",
 ## plot d1 snow totals
 plotThree <- table(snow$d1)
 barplot(plotThree, main="Day One Snow Totals",
-        xlab="d1", col=c("blue"))
+        xlab="Day One Total (inches)", ylab="Number of Occurences", col=c("blue"))
 
 ## plot d2 snow totals
 plotFour <- table(snow$d2)
 barplot(plotFour, main="Day Two Snow Totals",
-        xlab="d2", col=c("red"))
+        xlab="Day Two Total (inches)", ylab="Number of Occurences", col=c("red"))
 
 ## plot d3 snow totals
 plotFive <- table(snow$d3)
 barplot(plotFive, main="Day Three Snow Totals",
-        xlab="d3", col=c("blue"))
+        xlab="Day Three Total (inches)", ylab="Number of Occurences", col=c("blue"))
 
 ## plot d4 snow totals
 plotSix <- table(snow$d4)
 barplot(plotSix, main="Day Four Snow Totals",
-        xlab="d4", col=c("red"))
+        xlab="Day Four Total (inches)", ylab="Number of Occurences", col=c("red"))
 
 ## plot d5 snow totals
 plotSeven <- table(snow$d5)
 barplot(plotSeven, main="Day Five Snow Totals",
-        xlab="d5", col=c("blue"))
+        xlab="Day Five Total (inches)", ylab="Number of Occurences", col=c("blue"))
 
 ## plot d6 snow totals
 plotEight <- table(snow$d6)
 barplot(plotEight, main="Day Six Snow Totals",
-        xlab="d6", col=c("red"))
+        xlab="Day Six Total (inches)", ylab="Number of Occurences", col=c("red"))
 
 ## plot d7 snow totals
 plotNine <- table(snow$d7)
 barplot(plotNine, main="Day Seven Snow Totals",
-        xlab="d7", col=c("blue"))
+        xlab="Day Seven Total (inches)", ylab="Number of Occurences", col=c("blue"))
 
 ## plot d8 snow totals
 plotTen <- table(snow$d8)
 barplot(plotTen, main="Day Eight Snow Totals",
-        xlab="d8", col=c("red"))
+        xlab="Day Eight Total (inches)", ylab="Number of Occurences", col=c("red"))
 
 ## plot d9 snow totals
 plotEleven <- table(snow$d9)
 barplot(plotEleven, main="Day Nine Snow Totals",
-        xlab="d9", col=c("blue"))
+        xlab="Day Nine Total (inches)", ylab="Number of Occurences", col=c("blue"))
 
 ## plot d10 snow totals
 plotTwelve <- table(snow$d10)
 barplot(plotTwelve, main="Day Ten Snow Totals",
-        xlab="d10", col=c("red"))
+        xlab="Day Ten Total (inches)", ylab="Number of Occurences", col=c("red"))
